e95e755a9752acd560c000a7dd1b2f91199ac40e
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-11 15:58:57 +0100

    defined app routes for the game

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'games/new'
+  get 'new', to: 'games#new', as: 'new'
 
-  get 'games/score'
+  post 'score', to: 'games#score', as: 'score'
+
+  root to: 'games#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
