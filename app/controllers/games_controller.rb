bf2327aa1e4021fcf3f0018e71a3913836c92bfd
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-11 16:26:56 +0100

    created method to generate a grid of sample letters of a certain size

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,12 @@
 class GamesController < ApplicationController
   def new
+    @letters = generate_grid(10)
   end
 
   def score
+  end
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
   end
 end
