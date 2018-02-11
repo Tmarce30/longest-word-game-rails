1bcb272f65e77bc72f1d68e99a9f8890d7fab1c5
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-11 19:17:24 +0100

    created method to check if attempted word is english using wagon api

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,21 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = generate_grid(10)
+    @grid = generate_grid(10)
   end
 
   def score
-    raise
+    @my_attempt = params[:word]
+  end
+
+  private
+
+  def english_word?(word)
+    url = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(url.read)
+    return json['found']
   end
 
   def generate_grid(grid_size)
