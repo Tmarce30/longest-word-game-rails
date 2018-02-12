0d23614441fe3deadc1286c524a1b44ab13c296b
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-12 12:11:50 +0100

    created method included? to check if the guessed word correspond to the letters inside the grid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,9 +8,15 @@ class GamesController < ApplicationController
 
   def score
     @my_attempt = params[:word]
+    @grid = params[:my_grid]
+    included?(@my_attempt, @grid)
   end
 
   private
+
+  def included?(attempt, grid)
+    attempt.chars.all? { |letter| attempt.upcase.count(letter) <= grid.count(letter) }
+  end
 
   def english_word?(word)
     url = open("https://wagon-dictionary.herokuapp.com/#{word}")
